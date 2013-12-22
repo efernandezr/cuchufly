1274c0469547d47e761548953ea1abd2d5f043d1
Enrique Fernandez <efernandezr@gmail.com>
2013-12-22 19:14:50 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuchufly::Application.config.secret_key_base = '1a4bae441ae8588619ccacb14344fc5106691545a1bd359d1906cf0d4ee5343a370589fb3707f4c56df6bf6f92d72ab8157a9a1c3631d0f185e4948f086e04b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cuchufly::Application.config.session_store :cookie_store, key: '_cuchufly_session'
